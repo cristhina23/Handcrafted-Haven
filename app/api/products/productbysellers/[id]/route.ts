4e1a87f3259c0999bc1a74a18610e84c6eb7a476
Cristhina <viszellechacon@gmail.com>
2025-12-11 09:36:41 -0500

    update

--- app/api/products/productbysellers/[id]/route.ts
+++ app/api/products/productbysellers/[id]/route.ts
@@ -1,26 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db/db";
 import { getProductsBySellersId } from "@/lib/db/products";
 
-
 export async function GET(
-  req: NextRequest, 
-  { params }: { params: { id: string } } 
+  request: Request,
+  context: { params: { id: string } }
 ) {
   try {
     await connectDB();
 
-    
-    const sellerId = params.id;
-    console.log("Received sellerId:", sellerId)
+    const sellerId = context.params.id;
+    console.log("Received sellerId:", sellerId);
 
     const products = await getProductsBySellersId(sellerId);
 
     return NextResponse.json(products, { status: 200 });
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
-    console.log("API error from Get sellers products:", err);
+    console.error("API error from Get sellers products:", err);
     return NextResponse.json(
       { error: err.message },
       { status: 500 }
