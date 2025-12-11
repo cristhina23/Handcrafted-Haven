b6ebb3f21bca437f6f05b75ad4f7ca37006c4fe3
Cristhina <viszellechacon@gmail.com>
2025-12-11 13:18:31 -0500

    update

--- app/dashboard/orders/[id]/page.tsx
+++ app/dashboard/orders/[id]/page.tsx
@@ -1,5 +1,6 @@
 import UserOrderDetailsPage from "@/components/UserDashboard/UserOrderDetailsPage";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <UserOrderDetailsPage orderId={params.id} />;
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  return <UserOrderDetailsPage orderId={(await props.params).id} />;
 }
